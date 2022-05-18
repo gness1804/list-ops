af55666e15f1acd46f593922282c06ecb3250e23
gness1804 <graham.nessler@gmail.com>
2022-05-18 13:11:17 -0500

    pass tests for filter

--- list-ops.ts
+++ list-ops.ts
@@ -1,24 +1,21 @@
-export class List {
-  constructor(public items: number[] = []) {}
-  public static create(...values: number[] | List[]): List {
+export class List<T> {
+  constructor(public items: T[] = []) {}
+  public static create<T>(...values: T[] | List<T>[]): List<T> {
     // Do *not* construct any array literal ([]) in your solution.
     // Do *not* construct any arrays through new Array in your solution.
     // DO *not* use any of the Array.prototype methods in your solution.
 
     // You may use the destructuring and spreading (...) syntax from Iterable.
 
-    // @ts-ignore: it expects a number for isNaN but the item might be a List
-    if (values.length && isNaN(values[0])) {
+    if (values.length && values[0].items) {
       // the values are of type List
-      let resItems: number[] = [];
-      // @ts-ignore: list will always be a List even though TS thinks it might also be a number.
-      values.forEach((list: List) => {
+      let resItems: T[] = [];
+      values.forEach((list) => {
         resItems = [...resItems, ...list.items]
       })
       return new List(resItems);
     }
     // the values are of type number
-    // @ts-ignore: values will always be of type number even though TS thinks they might also be Lists
     return new List([...values])
   }
 
@@ -28,14 +25,25 @@ export class List {
   //   }
   // }
 
-  public append(otherList: List): number [] | List {
+  public append(otherList: List<T>): T [] | List<T> {
     const res = new List([...this.items, ...otherList.items]);
     // res.forEach(res.items)
     if (!this.items.length || !otherList.items.length) return res;
     return res.items
   }
 
-  public concat(otherList: List): number[] {
+  public concat(otherList: List<T>): T[] {
     return [...this.items, ...otherList.items]
+  }
+
+  public filter<T>(callback: (el: T) => boolean): T[] {
+    const res: T[] = []
+    for (let i = 0; i < this.items.length; i++) {
+      const item = this.items[i]
+      if (callback(item)) {
+        res.push(item);
+      }
+    }
+    return res;
   }
 }
