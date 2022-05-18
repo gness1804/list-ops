f897e044daac2098112decbfa1d2c749d6f6af2a
gness1804 <graham.nessler@gmail.com>
2022-05-18 10:40:01 -0500

    first few tests passing

--- list-ops.ts
+++ list-ops.ts
@@ -1,10 +1,24 @@
 export class List {
-  public static create(...values: unknown[]): unknown {
+  constructor(public items: number[] = []) {}
+  public static create(...values: number[]): List {
     // Do *not* construct any array literal ([]) in your solution.
     // Do *not* construct any arrays through new Array in your solution.
     // DO *not* use any of the Array.prototype methods in your solution.
 
     // You may use the destructuring and spreading (...) syntax from Iterable.
-    throw new Error('Remove this statement and implement this function')
+    return new List([...values])
+  }
+
+  private forEach(items: number[]) {
+    for (let i = 0; i < items.length; i++) {
+      console.log(items[i])
+    }
+  }
+
+  public append(otherList: List): number [] | List {
+    const res = new List([...this.items, ...otherList.items]);
+    res.forEach(res.items)
+    if (!this.items.length || !otherList.items.length) return res;
+    return res.items
   }
 }
